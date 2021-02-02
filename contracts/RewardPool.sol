6083babf2eabb99c6f283f9201e0df31da56c3ce
zuohuahua <zuohuahua@elastos.org>
2021-02-03 00:17:29 +0800

    add snapshot

--- contracts/RewardPool.sol
+++ contracts/RewardPool.sol
@@ -117,7 +117,145 @@ contract LPTokenWrapper {
     function getOutActualAmount(uint256 amount) private view returns (uint256) {
         return amount.mul(ratioMolecular).div(ratioDenominator);
     }
- }
+}
+
+pragma solidity ^0.5.0;
+
+contract LPTokenSnapshot is LPTokenWrapper {
+    using SafeMath for uint256;
+    using Arrays for uint256[];
+    using Counters for Counters.Counter;
+
+    // Snapshotted values have arrays of ids and the value corresponding to that id. These could be an array of a
+    // Snapshot struct, but that would impede usage of functions that work on an array.
+    struct Snapshots {
+        uint256[] ids;
+        uint256[] values;
+    }
+
+    mapping (address => Snapshots) private _accountBalanceSnapshots;
+    Snapshots private _totalSupplySnapshots;
+
+    // Snapshot ids increase monotonically, with the first value being 1. An id of 0 is invalid.
+    Counters.Counter private _currentSnapshotId;
+
+    /**
+     * @dev Emitted by {_snapshot} when a snapshot identified by `id` is created.
+     */
+    event Snapshot(uint256 id);
+
+    /**
+     * @dev Creates a new snapshot and returns its snapshot id.
+     *
+     * Emits a {Snapshot} event that contains the same id.
+     *
+     * {_snapshot} is `internal` and you have to decide how to expose it externally. Its usage may be restricted to a
+     * set of accounts, for example using {AccessControl}, or it may be open to the public.
+     *
+     * [WARNING]
+     * ====
+     * While an open way of calling {_snapshot} is required for certain trust minimization mechanisms such as forking,
+     * you must consider that it can potentially be used by attackers in two ways.
+     *
+     * First, it can be used to increase the cost of retrieval of values from snapshots, although it will grow
+     * logarithmically thus rendering this attack ineffective in the long term. Second, it can be used to target
+     * specific accounts and increase the cost of ERC20 transfers for them, in the ways specified in the Gas Costs
+     * section above.
+     *
+     * We haven't measured the actual numbers; if this is something you're interested in please reach out to us.
+     * ====
+     */
+    function _snapshot() internal returns (uint256) {
+        _currentSnapshotId.increment();
+
+        uint256 currentId = _currentSnapshotId.current();
+        emit Snapshot(currentId);
+        return currentId;
+    }
+
+    /**
+     * @dev Retrieves the balance of `account` at the time `snapshotId` was created.
+     */
+    function balanceOfAt(address account, uint256 snapshotId) public view returns (uint256) {
+        (bool snapshotted, uint256 value) = _valueAt(snapshotId, _accountBalanceSnapshots[account]);
+
+        return snapshotted ? value : balanceOf(account);
+    }
+
+    /**
+     * @dev Retrieves the total supply at the time `snapshotId` was created.
+     */
+    function totalSupplyAt(uint256 snapshotId) public view returns(uint256) {
+        (bool snapshotted, uint256 value) = _valueAt(snapshotId, _totalSupplySnapshots);
+
+        return snapshotted ? value : totalSupply();
+    }
+
+    function currentSnapshotId() public view returns(uint256) {
+        return _currentSnapshotId.current();
+    }
+
+    // Update balance and/or total supply snapshots before the values are modified.
+    function _beforeTokenTransfer(address account) internal {
+        _updateAccountSnapshot(account);
+        _updateTotalSupplySnapshot();
+    }
+
+    function _valueAt(uint256 snapshotId, Snapshots storage snapshots)
+        private view returns (bool, uint256)
+    {
+        require(snapshotId > 0, "LPTokenSnapshot: id is 0");
+        // solhint-disable-next-line max-line-length
+        require(snapshotId <= _currentSnapshotId.current(), "LPTokenSnapshot: nonexistent id");
+
+        // When a valid snapshot is queried, there are three possibilities:
+        //  a) The queried value was not modified after the snapshot was taken. Therefore, a snapshot entry was never
+        //  created for this id, and all stored snapshot ids are smaller than the requested one. The value that corresponds
+        //  to this id is the current one.
+        //  b) The queried value was modified after the snapshot was taken. Therefore, there will be an entry with the
+        //  requested id, and its value is the one to return.
+        //  c) More snapshots were created after the requested one, and the queried value was later modified. There will be
+        //  no entry for the requested id: the value that corresponds to it is that of the smallest snapshot id that is
+        //  larger than the requested one.
+        //
+        // In summary, we need to find an element in an array, returning the index of the smallest value that is larger if
+        // it is not found, unless said value doesn't exist (e.g. when all values are smaller). Arrays.findUpperBound does
+        // exactly this.
+
+        uint256 index = snapshots.ids.findUpperBound(snapshotId);
+
+        if (index == snapshots.ids.length) {
+            return (false, 0);
+        } else {
+            return (true, snapshots.values[index]);
+        }
+    }
+
+    function _updateAccountSnapshot(address account) private {
+        _updateSnapshot(_accountBalanceSnapshots[account], balanceOf(account));
+    }
+
+    function _updateTotalSupplySnapshot() private {
+        _updateSnapshot(_totalSupplySnapshots, totalSupply());
+    }
+
+    function _updateSnapshot(Snapshots storage snapshots, uint256 currentValue) private {
+        uint256 currentId = _currentSnapshotId.current();
+        if (_lastSnapshotId(snapshots.ids) < currentId) {
+            snapshots.ids.push(currentId);
+            snapshots.values.push(currentValue);
+        }
+    }
+
+    function _lastSnapshotId(uint256[] storage ids) private view returns (uint256) {
+        if (ids.length == 0) {
+            return 0;
+        } else {
+            return ids[ids.length - 1];
+        }
+    }
+}
+
 
 /*
 *   [Harvest]
@@ -126,7 +264,7 @@ contract LPTokenWrapper {
 *   by calling `notifyRewardAmount`
 */
 
-contract NoMintRewardPool is LPTokenWrapper, IRewardDistributionRecipient, Governable {
+contract NoMintRewardPool is LPTokenSnapshot, IRewardDistributionRecipient, Governable {
 
     using Address for address;
 
@@ -231,6 +369,8 @@ contract NoMintRewardPool is LPTokenWrapper, IRewardDistributionRecipient, Gover
         require(amount > 0, "Cannot stake 0");
         recordSmartContract();
 
+        _beforeTokenTransfer(msg.sender);
+
         super.stake(amount);
         emit Staked(msg.sender, amount);
     }
@@ -238,6 +378,7 @@ contract NoMintRewardPool is LPTokenWrapper, IRewardDistributionRecipient, Gover
     function withdraw(uint256 amount) public updateReward(msg.sender) {
         require(amount > 0, "Cannot withdraw 0");
         if (withdrawPeriod == 0) {
+            _beforeTokenTransfer(msg.sender);
             _withdraw(fixAmount(amount));
         } else {
             lockPool.withdraw(msg.sender, amount);
@@ -258,6 +399,9 @@ contract NoMintRewardPool is LPTokenWrapper, IRewardDistributionRecipient, Gover
     function withdrawApplication(uint256 amount) external updateReward(msg.sender) {
         require(amount > 0, "Cannot withdraw 0");
         require(withdrawPeriod != 0, "Withdraw period is 0, call withdraw directly");
+
+        _beforeTokenTransfer(msg.sender);
+
         lock(fixAmount(amount));
         emit WithdrawApplied(msg.sender, amount);
     }
@@ -307,6 +451,7 @@ contract NoMintRewardPool is LPTokenWrapper, IRewardDistributionRecipient, Gover
 
         if (block.timestamp >= periodFinish) {
             rewardRate = reward.div(duration);
+            _snapshot();
         } else {
             uint256 remaining = periodFinish.sub(block.timestamp);
             uint256 leftover = remaining.mul(rewardRate);
